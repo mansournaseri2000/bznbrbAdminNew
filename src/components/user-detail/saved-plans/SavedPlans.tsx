7868ebedd7d2e87f5ce283e08420f7f1baccb40b
Benyamin <benyaminardestanizadeh@outlook.com>
2024-10-03 19:26:14 +0330

    feat (user): define components for saved plans

--- src/components/user-detail/saved-plans/SavedPlans.tsx
+++ src/components/user-detail/saved-plans/SavedPlans.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 import { Flex } from '@/libs/primitives';
 
+import SavedPlansHero from './hero/SavedPlansHero';
+import SavedPlansList from './list/SavedPlansList';
+import SavedPlansPagination from './pagination/SavedPlansPagination';
+
 const SavedPlans = () => {
-  return <Flex>Saved Plans</Flex>;
+  return (
+    <Flex width={'100'} direction={'column'} gap={'4'}>
+      <SavedPlansHero />
+      <SavedPlansList />
+      <SavedPlansPagination />
+    </Flex>
+  );
 };
 
 export default SavedPlans;
