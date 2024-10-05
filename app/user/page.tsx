9725578b2460b5eadc99e7618cfeff4c9d8daf30
Benyamin <benyaminardestanizadeh@outlook.com>
2024-10-05 11:37:42 +0330

    style(user): remove user info modal from user index page

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import UserHero from '@/components/user/hero/UserHero';
-import UserInfoModal from '@/components/user/info-modal/UserInfoModal';
 import UserList from '@/components/user/list/UserList';
 import UserPagination from '@/components/user/pagination/UserPagination';
 import AppFlex from '@/libs/primitives/layout/Flex';
@@ -10,7 +9,6 @@ export default function User() {
   return (
     <AppFlex width={'100%'} direction={'column'} gap={'4'}>
       <UserHero />
-      <UserInfoModal />
       <UserList />
       <UserPagination />
     </AppFlex>
