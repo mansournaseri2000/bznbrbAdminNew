e92ab5477eda529dbd19b63f7162b103abaa2672
Benyamin <benyaminardestanizadeh@outlook.com>
2024-10-07 11:56:28 +0330

    feat(article): implement article

--- src/components/article/hero/ArticleHero.tsx
+++ src/components/article/hero/ArticleHero.tsx
@@ -5,8 +5,8 @@ import { Button, Grid, TextField } from '@/libs/primitives';
 const ArticleHero = () => {
   return (
     <Grid width={'100%'} columns={'2'} gapX={'5'} style={{ gridTemplateColumns: '0.3fr 3fr' }}>
-      <Button>افزودن مقاله</Button>
-      <TextField placeholder='جستجوی عنوان مقاله' />
+      <Button>افزودن مقاله </Button>
+      <TextField placeholder='جستجوی عنوان  مقاله' />
     </Grid>
   );
 };
