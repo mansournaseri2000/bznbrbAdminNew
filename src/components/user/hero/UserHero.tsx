df7dc3fb6195b021e15138ccdd194017761d1442
Benyamin <benyaminardestanizadeh@outlook.com>
2024-10-07 12:01:02 +0330

    feat(user): implement user

--- src/components/user/hero/UserHero.tsx
+++ src/components/user/hero/UserHero.tsx
@@ -9,7 +9,7 @@ const UserHero = () => {
   return (
     <AppGrid width={'100%'} columns={'3'} gapX={'4'} style={{ gridTemplateColumns: '0.5fr 3fr 1fr' }}>
       <AppButton size={'3'}>ثبت کاربر</AppButton>
-      <TextField placeholder='جستجوی کاربر' style={{ borderRadius: 12 }} />
+      <TextField placeholder='جستجوی  کاربر' style={{ borderRadius: 12 }} />
       <AppFlex align={'center'} justify={'center'} style={{ border: '1px solid #D4D4D4', borderRadius: 12 }}>
         نوع کاربر
       </AppFlex>
