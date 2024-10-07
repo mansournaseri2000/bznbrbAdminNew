90f891655fcd330e9d3b4ac0a7e7c643788fee1c
Ali Hasani <hasaninft1992@gmail.com>
2024-10-07 13:16:53 +0330

    fix: modify layout

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,9 +1,21 @@
 import { ReactNode } from 'react';
 
+import { Flex } from '@/libs/primitives';
+
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <main style={{ paddingBlock: '24px' }}>{children}</main>
+      <header style={{ border: '1px solid red', padding: '16px' }}>header</header>
+      <main>
+        <Flex style={{ border: '1px solid blue' }}>
+          <Flex width={'30%'} style={{ border: '1px solid red' }}>
+            menu
+          </Flex>
+          <Flex width={'70%'} style={{ border: '1px solid red' }}>
+            {children}
+          </Flex>
+        </Flex>
+      </main>
     </>
   );
 };
