05284c40b1e0c042baf2173141c03161327ac8b5
Benyamin <benyaminardestanizadeh@outlook.com>
2024-11-12 17:15:26 +0330

    haj ali codo begir

--- src/api/data-management.ts
+++ src/api/data-management.ts
@@ -6,7 +6,8 @@ import { ApiData } from './types';
 export const getPointByParams = async (obj: Record<string, any>) => {
   console.log(handleQueryParams(obj), 'DATA');
   try {
-    const res = await ApiManagerV2.get<ApiData<PlaceListResponse>>(`places/search?${handleQueryParams(obj)}`);
+    const queryString = handleQueryParams(obj);
+    const res = await ApiManagerV2.get<ApiData<PlaceListResponse>>(`places/search?${queryString.toString()}`);
     return res.data.data;
   } catch (error) {
     console.error('Error fetching places:', error);
@@ -17,7 +18,16 @@ export const getPointByParams = async (obj: Record<string, any>) => {
 const handleQueryParams = (obj: Record<string, any>) => {
   const queryParams = Object.entries(obj)
     .filter(([_, value]) => value !== '' && value !== null && value !== undefined)
-    .map(([key, value]) => `${(key)}=${(value)}`);
+    .map(([key, value]) => `${key}=${value}`);
 
   return queryParams.length ? `${queryParams.join('&')}` : '';
+  // const queryParams = new URLSearchParams(
+  //   Object.entries(obj).reduce((acc, [key, value]) => {
+  //     if (value !== '' && value !== null && value !== undefined) {
+  //       acc[key] = value;
+  //     }
+  //     return acc;
+  //   }, {})
+  // );
+  // return queryParams.toString();
 };
